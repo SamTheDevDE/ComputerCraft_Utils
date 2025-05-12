2dc866a2fa4f1fb8482f78e8315bf1dfdae01b3d
SamTheDevDE <samthedev@lumix.bot>
2025-05-12 19:28:39 +0200

    added example chatbox structure

---
+++ example.lua
@@ -0,0 +1,23 @@
+-- Wrap the chat box peripheral
+local chat = peripheral.find("chatBox")
+if not chat then
+  error("No Chat Box connected!")
+end
+
+print("ChatBot is running...")
+
+-- Main loop
+while true do
+  local event, username, message, uuid = os.pullEvent("chat")
+  
+  -- Debug print
+  print("[" .. username .. "]: " .. message)
+  
+  -- Command handler
+  if message == "!test" then
+    chat.sendMessage("Hello, " .. username .. "! This is a test response.")
+  elseif message == "!time" then
+    local time = os.time()
+    chat.sendMessage("Current in-game time is: " .. time)
+  end
+end
