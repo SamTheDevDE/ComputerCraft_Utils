6caa510a001a29a1a744645361d8369f2f75db34
Samuel Domke <120846365+SamTheDevDE@users.noreply.github.com>
2025-05-12 21:19:55 +0200

    Update pcn.lua

--- pcn.lua
+++ pcn.lua
@@ -1,17 +1,41 @@
 local basalt = require("basalt")
 
 -- Get a reference to the monitor
-local monitor = peripheral.find("monitor") 
--- Or specific side: peripheral.wrap("right")
+local monitor = peripheral.find("monitor")
 
 -- Create frame for monitor
-local monitorFrame = basalt.createFrame():setTerm(monitor) -- :setTerm is the important method here
+local monitorFrame = basalt.createFrame():setTerm(monitor)
 
--- Add elements like normal
+-- Title label
+monitorFrame:addLabel()
+    :setText("Login")
+    :setPosition(10, 2)
+    :setForeground(colors.yellow)
+    :setBackground(colors.blue)
+
+-- Username label and input
+monitorFrame:addLabel()
+    :setText("Username:")
+    :setPosition(5, 5)
+monitorFrame:addInput()
+    :setPosition(15, 5)
+    :setSize(15, 1)
+    :setDefaultText("")
+
+-- Password label and input
+monitorFrame:addLabel()
+    :setText("Password:")
+    :setPosition(5, 7)
+monitorFrame:addInput()
+    :setPosition(15, 7)
+    :setSize(15, 1)
+    :setInputType("password")
+
+-- Login button
 monitorFrame:addButton()
-    :setText("Monitor Button")
-    :setWidth(24)
-    :setPosition(2, 2)
+    :setText("Login")
+    :setPosition(12, 10)
+    :setSize(10, 1)
 
 -- Start Basalt
 basalt.run()
